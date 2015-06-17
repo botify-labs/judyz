ee00e2a09cea75cd61ddfd0b35bf5d8682eadbe7
zeb <yves@botify.com>
2015-06-17 16:09:07 +0200

    Add missing files to judyz_cffi via MANIFEST.in

--- src/judyzcffi/setup.py
+++ src/judyzcffi/setup.py
@@ -2,17 +2,13 @@ from setuptools import setup
 
 setup(
     name="judyz-cffi",
-    version="0.8.0",
+    version="0.8.1",
     py_modules=["judyz_cffi"],
     author="Yves Bastide",
     author_email="stid@acm.org",
     description="A CFFI Judy wrapper",
     license="MIT",
     url="https://github.com/botify-labs/judyz",
-    ext_modules=[
-        # none
-        # judy_cffi._ffi.verifier.get_extension(),
-    ],
     setup_requires=["cffi>=1.0.0"],
     cffi_modules=["judy_cffi_build.py:ffi"],
     install_requires=["cffi>=1.0.0"],
